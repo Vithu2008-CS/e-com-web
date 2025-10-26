60a7e0ea98eee029cee48d847d8654729749cb98
Vithu2008-CS <thangavithu@gmail.com>
2025-10-26 19:08:25 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+        <div className="d-flex align-items-center justify-content-center pb-5">
+          <p className="mb-0">© 2025 Your Company. All rights reserved.</p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Footer, Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+          Launching an e-commerce store for clothing, jewelry, and electronics requires a strategic blend of clear branding and operational excellence. To stand out
+          in a competitive market, you must define a unique selling proposition, such as focusing on sustainable products, affordable trends, or luxury goods. Your
+          website needs a professional design with high-quality images, especially for jewelry and apparel, and detailed descriptions with size charts and tech specs
+          to build customer trust. Sourcing reliable suppliers is critical, particularly for electronics to ensure authenticity. Finally, a robust marketing plan using visual
+          platforms like Instagram and TikTok for fashion and jewelry, combined with SEO-driven content for electronics, will drive traffic and sales, while a clear andfair return policy will ensure customer satisfaction and loyalty.
+        </p>
+
+        <h2 className="text-center py-4">Our Products</h2>
+        <div className="row">
+          <div className="col-md-4 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Mens's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Women's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Electronics</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default AboutPage
